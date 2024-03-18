af0defc01be8501074d8d64dd6f6021e255524fd
Elvis Pérez Pérez <140919160+AnythingStore@users.noreply.github.com>
2024-03-18 17:38:11 -0400

    renderizado a canvaskit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,8 +30,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "0c6d16dbe2a1272892f4387a051d6e31
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e8794c04912637726ea8dc868975a12e",
-"/": "e8794c04912637726ea8dc868975a12e",
+"index.html": "f36768f277795a98634c9ce687d5e0e9",
+"/": "f36768f277795a98634c9ce687d5e0e9",
 "main.dart.js": "7682133c3221016d61937c8738dfe454",
 "manifest.json": "24916695f0a2233b43b7b2fbbbd784cd",
 "version.json": "097d7d22327e4daa89237b4d4ec22337"};
